1100ec2f57bd0291a9178aa7a326b46baa663819
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-02-09 16:32:04 +0100

    Update scraper

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import datetime
 import re
+import os
 import json
 from collections import defaultdict
 
@@ -12,47 +13,64 @@ class BGBLScraper(object):
     BASE_URL = 'https://www.bgbl.de/xaver/bgbl/'
     START = 'start.xav?startbk=Bundesanzeiger_BGBl'
     BASE_TOC = 'ajax.xav?q=toclevel&n=0'
-    AJAX = 'ajax.xav?q=toclevel&bk=bgbl&n={}'
+    AJAX = 'ajax.xav?q=toclevel&bk=bgbl&n={docid}'
+    PDF = 'media/{}'
     TEXT = ('text.xav?SID=&tf=xaver.component.Text_0&tocf='
             '&qmf=&hlf=xaver.component.Hitlist_0'
-            '&bk=bgbl&start=%2F%2F*%5B%40node_id%3D%27{docid}%27%5D')
+            '&bk=bgbl&start=%2F%2F*%5B%40node_id%3D%27{did}%27%5D'
+            '&tocid={docid}')
+    PDF_VIEWER = ('text.xav?SID=&tf=xaver.component.Text_0&tocf=&qmf=&'
+        'hlf=xaver.component.Hitlist_0&bk=bgbl&start='
+        '%2F%2F*%5B%40node_id%3D%27{fragment}%27%5D&skin=pdf'
+    )
+    PDF_META = ('text.xav?SID=&start=%2F%2F*[%40node_id%3D%27{fragment}%27]&'
+                'skin=&tf=xaver.component.Text_0&hlf=xaver.component.Hitlist_0')
+    PDF_REDIRECT = 'media.xav/bgbl{part}_{year}_{num}.pdf?SID=&name=..%2Fdocuments%2Fbgbl{part}_{year}_{num}.pdf&iid={docid}'
 
     year_toc = defaultdict(dict)
     year_docs = defaultdict(dict)
     toc = {}
 
-    def __init__(self, part_count=2):
+    def __init__(self, min_year=1949, max_year=10000,
+                 create_document=True, document_path=None, part_count=2):
+        self.document_path = document_path
         self.login()
+        self.max_year = max_year
+        self.min_year = min_year
         self.part_count = part_count
 
     def login(self):
         self.session = requests.Session()
         self.session.get(self.BASE_URL + self.START)
 
-    def get(self, url):
+    def get(self, url, **kwargs):
         while True:
-            response = self.session.get(url)
-            if 'Session veraltet' in response.text:
+            print(url)
+            response = self.session.get(url, **kwargs)
+            if not kwargs and 'Session veraltet' in response.text:
+                print('Session expired...')
                 self.login()
                 continue
             return response
 
-    def scrape(self, low=0, high=10000):
+    def get_download_dir(self, part, year, number):
+        path = os.path.join(self.document_path, str(part), str(year))
+        os.makedirs(path, exist_ok=True)
+        return path
+
+    def scrape(self):
         self.toc_offsets = self.get_base_toc()
         for part in range(1, self.part_count + 1):
             print(part)
-            self.get_main_toc(part)
-            self.get_all_year_tocs(part, low, high)
-            yield from self.get_all_tocs(part, low, high)
+            yield from self.get_main_toc_part(part)
 
     def get_json(self, url):
-        response = self.get(url)
+        response = self.get(self.BASE_URL + url)
         response.encoding = 'utf-8'
         return json.loads(response.text)
 
     def get_base_toc(self):
-        url = self.BASE_URL + self.BASE_TOC
-        doc = self.get_json(url)
+        doc = self.get_json(self.BASE_TOC)
         items = doc['items'][0]['c']
         toc_offsets = []
         for item in items:
@@ -61,12 +79,9 @@ class BGBLScraper(object):
             toc_offsets.append(item['id'])
         return toc_offsets
 
-    def get_main_toc(self, part=1):
-        self.get_main_toc_part(part)
-
     def get_main_toc_part(self, part):
         offset = self.toc_offsets[part - 1]
-        url = self.BASE_URL + self.AJAX.format(offset)
+        url = self.AJAX.format(docid=offset)
         doc = self.get_json(url)
         items = doc['items'][0]['c']
         for item in items:
@@ -74,20 +89,15 @@ class BGBLScraper(object):
                 year = int(item['l'])
             except ValueError:
                 continue
-            doc_id = item['id']
-            if doc_id is not None:
-                self.year_toc[part][year] = doc_id
-
-    def get_all_year_tocs(self, part=1, low=0, high=10000):
-        for year in self.year_toc[part]:
-            if not (low <= year <= high):
+            if not (self.min_year <= year <= self.max_year):
                 continue
-            print("Getting Year TOC %d for %d" % (year, part))
-            self.get_year_toc(part, year)
+            self.login()
+            yield from self.get_year_toc(part, year, item)
 
-    def get_year_toc(self, part, year):
-        year_doc_id = self.year_toc[part][year]
-        url = self.BASE_URL + self.AJAX.format(year_doc_id)
+    def get_year_toc(self, part, year, doc_item):
+        print("Getting Year TOC %d for %d" % (year, part))
+        year_doc_id = doc_item['id']
+        url = self.AJAX.format(docid=year_doc_id)
         doc = self.get_json(url)
         items = doc['items'][0]['c']
         for item in items:
@@ -95,31 +105,24 @@ class BGBLScraper(object):
                               item['l'])
             if match is None:
                 continue
-            print(item['l'])
             number = int(match.group(1))
-            date = match.group(2)
-            doc_id = item['id']
-            self.year_docs[part].setdefault(year, {})
-            self.year_docs[part][year][number] = {
-                'date': date,
-                'doc_id': doc_id
-            }
+            yield from self.get_toc(part, year, number, item)
 
-    def get_all_tocs(self, part=1, low=0, high=10000):
-        for year in self.year_docs[part]:
-            if not (low <= year <= high):
-                continue
-            for number in self.year_docs[part][year]:
-                yield from self.get_toc(part, year, number)
+    def get_toc(self, part, year, number, item):
+        # self.login()
+        # import ipdb; ipdb.set_trace()
 
-    def get_toc(self, part, year, number):
-        year_doc = self.year_docs[part][year][number]
-        doc_id = year_doc['doc_id']
-        url = self.BASE_URL + self.TEXT.format(docid=doc_id)
+        url = self.AJAX.format(docid=item['id'])
         doc = self.get_json(url)
+        url = self.TEXT.format(did=item['did'], docid=item['id'])
+        doc = self.get_json(url)
+        doc_url = None
+        if self.create_document:
+            doc_url = self.download_document(part, year, number, doc)
+
         root = lxml.html.fromstring(doc['innerhtml'])
         order_num = 1
-        for tr in root.cssselect('tr'):
+        for tr in root.xpath('//table[1]//tr'):
             td = tr.cssselect('td')[1]
             divs = td.cssselect('div')
             law_date = None
@@ -134,30 +137,60 @@ class BGBLScraper(object):
             href = link.attrib['href']
             text = divs[2].text_content().strip()
             print(text)
-            match = re.search('aus +Nr. +(\d+) +vom +(\d{1,2}\.\d{1,2}\.\d{4}),'
-                              ' +Seite *(\d*)\w?\.?$', text)
+            match = re.search(r'aus +Nr. +(\d+) +vom +(\d{1,2}\.\d{1,2}\.\d{4}),'
+                              r' +Seite *(\d*)\w?\.?$', text)
             page = None
             date = match.group(2)
             if match.group(3):
                 page = int(match.group(3))
             kind = 'entry'
-            if name in ('Komplette Ausgabe', 'Inhaltsverzeichnis'):
-                # FIXME: there are sometimes more meta rows
+            if (name in ('Komplette Ausgabe', 'Inhaltsverzeichnis') or
+                    name.startswith('Hinweis: ')):
                 kind = 'meta'
             yield {
                 'row_id': '{}_{}_{}_{}'.format(part, year, number, order_num),
                 'part': part, 'order': order_num,
-                'year': year, 'toc_doc_id': doc_id,
+                'year': year, 'toc_doc_id': item['id'], 'doc_did': item['did'],
                 'number': number, 'date': date,
                 'law_date': law_date, 'kind': kind,
-                'name': name, 'href': href, 'page': page
+                'name': name, 'href': href, 'page': page,
+                'doc_url': doc_url
             }
             order_num += 1
+
+    def download_document(self, part, year, number, doc):
+        print('Download document', part, year, number)
+        fragment = doc['nextfragment']
+        pdf_viwer_url = self.BASE_URL + self.PDF_VIEWER.format(fragment=fragment)
+        # Set session state to retrieve URL
+        response = self.get(pdf_viwer_url)
+        match = re.search(r'iid=(\d+)"', response.text)
+        docid = match.group(1)
+        url = self.BASE_URL + self.PDF_REDIRECT.format(
+            part=part, year=year, num=number, docid=docid,
+        )
+        response = self.get(url, stream=True)
+        if response.status_code == 200:
+            if self.document_path:
+                path = self.get_download_dir(part, year, number)
+                path = os.path.join(path, '%s.pdf' % number)
+                with open(path, 'wb') as f:
+                    for chunk in response:
+                        f.write(chunk)
+            print(response.url)
+            return response.url
 
 
 if __name__ == '__main__':
     db = dataset.connect('sqlite:///data.sqlite')
     table = db['data']
-    bgbl = BGBLScraper()
-    for item in bgbl.scrape(1949, datetime.datetime.now().year):
+    documents = None
+    if os.path.exists('documents'):
+        documents = 'documents/'
+    bgbl = BGBLScraper(
+        min_year=1949,
+        max_year=datetime.datetime.now().year,
+        document_path=documents
+    )
+    for item in bgbl.scrape():
         table.upsert(item, ['row_id'])
